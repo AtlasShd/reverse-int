b3340a1bba0990377f4db99ceb3dfd3b5ef3f878
AtlasShd <tlsshrugged@gmail.com>
2022-01-15 19:52:16 +0300

    feat: add solution of reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+'use strict';
+
+module.exports = function reverse(n) {
+    let result = '';
+    Array.from(String(Math.abs(n))).forEach((i) => {
+        result = i + result;
+    });
+    return result;
 }
